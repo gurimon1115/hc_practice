b56bef5692a7a76a595683669f773e44f7c4f4b4
gurimon <haruk.haruk1115@gmail.com>
2023-11-28 21:15:19 +0900

    リファクタリング

--- ruby/random.rb
+++ ruby/random.rb
@@ -1,11 +1,8 @@
 members = %w[A B C D E F]
-pattern = %w[3-3 2-4]
+pattern = [2, 3].sample
 
-members.shuffle!
-if pattern.sample == '3-3'
-  p members[0, 3].sort
-  p members[3, 6].sort
-else
-  p members[0, 2].sort
-  p members[2, 6].sort
-end
+group1 = members.sample(pattern).sort
+group2 = members - group1
+
+p group1
+p group2

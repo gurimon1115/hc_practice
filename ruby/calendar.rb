a8146c8c94b76c5dec4c2bebad1a8451e5a75174
gurimon <haruk.haruk1115@gmail.com>
2023-12-01 09:15:48 +0900

    リファクタリング

--- ruby/calendar.rb
+++ ruby/calendar.rb
@@ -2,6 +2,7 @@ require 'optparse'
 require 'date'
 
 date = Date.today
+year = date.year
 
 # 引数-mとintの引数を受付ける
 value = date.month
@@ -11,11 +12,10 @@ opt.parse!(ARGV)
 
 raise "#{value} is neither a month number (1..12) nor a name" unless value.between?(1, 12)
 
-first_day = Date.new(date.year, value)
-last_day = Date.new(date.year, value, -1)
+first_day = Date.new(year, value)
+last_day = Date.new(year, value, -1)
 
 days = %w[月 火 水 木 金 土 日]
-month_start_day = first_day.wday - 1
 
 # 月初から月末の日付オブジェクトを取得する
 dates = []
@@ -24,24 +24,18 @@ dates = []
 end
 
 # 年月、月〜日を表示
-puts "      #{first_day.strftime('%m')}月 #{date.year}"
+puts "      #{first_day.strftime('%m')}月 #{year}"
 puts days.join(' ')
 
 # 日付ごとの処理
-idx = 0
 text = ''
-dates.each do |dd|
+(first_day..last_day).each do |dd|
   # 月初の曜日に合わせてインデントする
-  text = '   ' * month_start_day if idx.zero? && month_start_day >= 1
+  text = '   ' * (dd.wday - 1) if dd.day == 1 && dd.wday != 0
   # 数字が1桁か2桁に合わせてインデントする
-  text += if dd.mday.to_s.length == 2
-            "#{dd.mday} "
-          else
-            " #{dd.mday} "
-          end
+  text += dd.mday.to_s.rjust(2)
   # 日曜なら改行する
-  text += "\n" if dd.wday.zero?
-  idx += 1
+  text += dd.wday.zero? ? "\n" : ' '
 end
 
 puts text

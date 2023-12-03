be51b2f238a9bbd2484596167576c1b2fd0f3d0f
gurimon <haruk.haruk1115@gmail.com>
2023-12-03 12:29:57 +0900

    リファクタリング

--- ruby/golf_score.rb
+++ ruby/golf_score.rb
@@ -7,16 +7,24 @@ puts inputs.length
 regulations = inputs[0].split(',').map(&:to_i)
 achievements = inputs[1].split(',').map(&:to_i)
 
+SCORE_MAPPING = {
+  -4 => 'コンドル',
+  -3 => 'アルバトロス',
+  -2 => 'イーグル',
+  -1 => 'バーディ',
+  0 => 'パー',
+  1 => 'ボギ-'
+}
+
 scores = regulations.map.with_index do |x, i|
   # 規定打数 - 実績
   achievement = achievements[i]
   score = achievement - x
-  if achievement == 1 then x == 5 ? 'コンドル' : 'ホールインワン'
-  elsif score == -3 && x == 5 then 'アルバトロス'
-  elsif score == -2 then 'イーグル'
-  elsif score == -1 then 'バーディ'
-  elsif score.zero? then 'パー'
-  elsif score >= 1 then score >= 2 ? "#{score}ボギー" : 'ボギー'
+
+  if achievement == 1 && score != -4 then 'ホールインワン'
+  elsif score >= 2 then "#{score}ボギー"
+  else
+    SCORE_MAPPING[score]
   end
 end
 
